ae1bcd164ac262d53d2bbc95b7e3dfdff8e6b70c
Yoo Chung <dev@chungyc.org>
2023-01-29 23:34:03 +0000

    Adjust colors for latest update depending on mode.

--- src/Web/Site/Styles/Front.hs
+++ src/Web/Site/Styles/Front.hs
@@ -6,7 +6,7 @@
 module Web.Site.Styles.Front (style) where
 
 import Clay hiding (style)
-import Prelude hiding (div)
+import Clay.Media qualified as Media
 
 -- |
 -- Style for front page which Clay will render into a stylesheet.
@@ -15,5 +15,13 @@ style = do
   ".latest-update" ? do
     sym margin $ em 2
     sym padding $ em 2
-    border (px 2) solid $ rgb 20 20 50
-    backgroundColor $ rgb 250 250 250
+
+  query Media.all [Media.prefersColorScheme Media.light] $ do
+    ".latest-update" ? do
+      border (px 2) solid $ rgb 20 20 50
+      backgroundColor $ rgb 250 250 250
+
+  query Media.all [Media.prefersColorScheme Media.dark] $ do
+    ".latest-update" ? do
+      border (px 2) solid $ rgb 230 230 200
+      backgroundColor $ rgb 5 5 5
