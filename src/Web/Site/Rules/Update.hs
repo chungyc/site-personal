b46b3db03457fe223fb9484e63ff4311fb079f03
Yoo Chung <dev@chungyc.org>
2023-01-29 20:12:32 +0000

    Add updates feed.

--- src/Web/Site/Rules/Update.hs
+++ src/Web/Site/Rules/Update.hs
@@ -12,6 +12,7 @@ import Web.Site.Routes
 -- Rules related to generated recent updates from me.
 rules :: Rules ()
 rules = do
+  -- The overview page.
   create ["updates"] $ do
     route idRoute
     compile $ do
@@ -19,6 +20,7 @@ rules = do
       let updatesContext =
             listField "updates" defaultContext (return updates)
               <> constField "title" "Updates"
+              <> constField "rss-feed-link" "/updates.rss"
               <> defaultContext
 
       makeItem ""
@@ -26,6 +28,7 @@ rules = do
         >>= loadAndApplyTemplate "templates/default.html" updatesContext
         >>= relativizeUrls
 
+  -- Individual update page.
   match "update/**" $ do
     route stripExtension
     compile $
@@ -35,6 +38,15 @@ rules = do
         >>= loadAndApplyTemplate "templates/default.html" defaultContext
         >>= relativizeUrls
 
+  -- RSS feed for updates.
+  create ["updates.rss"] $ do
+    route idRoute
+    compile $ do
+      let feedContext = bodyField "description" <> defaultContext
+      posts <- recentFirst =<< loadAllSnapshots "update/**" "content"
+      renderRss updateFeedConfiguration feedContext posts
+
+-- | Apply a context with the latest update in the list field @latest-update@ to the given rule.
 withLatest :: (Context String -> Compiler (Item String)) -> Compiler (Item String)
 withLatest f = do
   updates <- fmap (take 1) . recentFirst =<< loadAllSnapshots "update/**" "content"
@@ -43,3 +55,14 @@ withLatest f = do
           then defaultContext
           else listField "latest-update" defaultContext (return updates) <> defaultContext
   f indexContext
+
+-- | Feed configuration for updates.
+updateFeedConfiguration :: FeedConfiguration
+updateFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "Updates for Yoo Chung",
+      feedDescription = "Occasional personal updates from Yoo Chung.",
+      feedAuthorName = "Yoo Chung",
+      feedAuthorEmail = "web@chungyc.org",
+      feedRoot = "https://chungyc.org/updates"
+    }
