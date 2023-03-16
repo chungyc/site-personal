f5219d9d866ffe034b5d19a2fada7d4b5559bee9
Yoo Chung <dev@chungyc.org>
2023-03-16 18:58:08 +0000

    Clean up module documentation.

--- src/Web/Site/Rules/Diagram.hs
+++ src/Web/Site/Rules/Diagram.hs
@@ -7,29 +7,58 @@ module Web.Site.Rules.Diagram (rules) where
 
 import Data.List (intercalate)
 import Hakyll
+import Web.Site.Compilers
 
--- import           Web.Site.Compilers
-
--- | Rules related to images generated from Haskell code based on Diagrams.
+-- |
+-- Rules related to images generated from Haskell code based on Diagrams.
+--
+-- The Haskell code should not have to import 'Diagrams.Prelude' or
+-- "Diagrams.Runner.putDiagram".  These will be implicitly imported by
+-- prepending boilerplate code.
+--
+-- This allows the Haskell code for generating diagrams to only have
+-- to define a main function which writes out the diagram, along
+-- with any other functions necessary to generate the diagram.
+-- For example,
+--
+-- @
+--    main = putDiagram defaultOptions roundThing
+--
+--    roundThing :: Diagram B
+--    roundThing = circle 1 # lc red
+-- @
 rules :: Rules ()
 rules = do
   match "diagrams/**.hs" $ do
     route $ setExtension "svg"
-    compile haskellCompiler'
-  where
-    haskellCompiler' = do
-      getResourceString
-        >>= return . fmap (preamble ++)
-        >>= withItemBody
-          ( unixFilter
-              "stack"
-              [ "runhaskell",
-                "--",
-                "-XGHC2021",
-                "-XOverloadedStrings"
-              ]
-          )
+    compile $
+      getResourceBody
+        >>= pure . fmap (preamble ++)
+        >>= makeItem
+        >> haskellCompiler
 
+-- |
+-- Common preamble to Diagrams-based code.
+--
+-- This will be used to remove common boilerplate from Haskell code
+-- which are used to generate diagrams for this web site.
+-- The preamble will import the 'Diagrams.Runner' module,
+-- which basically re-exports 'Diagrams.Prelude'.
+-- It will also export the "Diagrams.Runner.putDiagram" function,
+-- which will be responsible for writing out a diagram in SVG format
+-- to standard output.
+--
+-- This allows the Haskell code for generating diagrams to only have
+-- to define a main function which writes out the diagram, along
+-- with any other functions necessary to generate the diagram.
+-- For example,
+--
+-- @
+--    main = putDiagram defaultOptions roundThing
+--
+--    roundThing :: Diagram B
+--    roundThing = circle 1 # lc red
+-- @
 preamble :: String
 preamble =
   intercalate
