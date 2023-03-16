6b0a80e77eee516f34627323e36cac608d16ac0c
Yoo Chung <dev@chungyc.org>
2023-03-16 18:20:15 +0000

    Downgrade to Stack LTS 20.12 to regain HLS support.

--- src/Diagrams/Runner.hs
+++ src/Diagrams/Runner.hs
@@ -20,8 +20,14 @@ import Diagrams.Prelude
 import Graphics.Svg
 import Prelude hiding (putStr)
 
+{-|
+Write out SVG for the given diagram to standard output.
+-}
 putDiagram :: (SVGFloat n) => Options SVG V2 n -> QDiagram SVG V2 n Any -> IO ()
 putDiagram options diagram = putStr $ prettyText $ renderDia SVG options $ pad 1.1 $ diagram
 
+{-|
+Default options for rendering a diagram into SVG.
+-}
 defaultOptions :: (SVGFloat n) => Options SVG V2 n
 defaultOptions = SVGOptions (mkWidth 400) Nothing "" [] True
