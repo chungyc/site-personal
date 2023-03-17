3098ecca25c7c1859d176ed2167272131abcfb2b
Yoo Chung <dev@chungyc.org>
2023-03-17 15:28:10 -0400

    Create syntax highlighting stylesheets from rules.
    The code for generating them will almost never change, so there is not much point in having separate code files with only a few lines generate them dynamically.

--- src/Web/Site/Rules/Stylesheet.hs
+++ src/Web/Site/Rules/Stylesheet.hs
@@ -5,7 +5,9 @@
 -- Maintainer: web@chungyc.org
 module Web.Site.Rules.Stylesheet (rules) where
 
+import Data.List (intercalate)
 import Hakyll
+import Text.Pandoc.Highlighting (pygments, styleToCss, zenburn)
 import Web.Site.Compilers
 
 -- | Rules related to stylesheets generated from Haskell code.
@@ -14,3 +16,26 @@ rules = do
   match "css/**.hs" $ do
     route $ setExtension "css"
     compile $ getResourceBody >>= haskellCompiler
+
+  -- Stylesheet for supporting syntax highlighting.
+  -- This will import the actual stylesheet according to the preferred color scheme.
+  create ["css/syntax.css"] $ do
+    route idRoute
+    compile $
+      makeItem $
+        intercalate
+          "\n"
+          [ "@import \"syntax-light.css\" all and (prefers-color-scheme: light);",
+            "@import \"syntax-dark.css\" all and (prefers-color-scheme: dark);",
+            ""
+          ]
+
+  -- Syntax highlighting in light mode.
+  create ["css/syntax-light.css"] $ do
+    route idRoute
+    compile $ makeItem $ styleToCss pygments
+
+  -- Syntax highlighting in dark mode.
+  create ["css/syntax-dark.css"] $ do
+    route idRoute
+    compile $ makeItem $ styleToCss zenburn
