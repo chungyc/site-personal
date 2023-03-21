0138521fcbf3e26dc065c8d57cce120997602ef0
Yoo Chung <dev@chungyc.org>
2023-03-20 22:13:37 -0400

    More documentation for Web.Site.Rules.Diagram module.

--- src/Web/Site/Rules/Diagram.hs
+++ src/Web/Site/Rules/Diagram.hs
@@ -3,29 +3,47 @@
 -- Copyright: Copyright (C) 2023 Yoo Chung
 -- License: All rights reserved
 -- Maintainer: web@chungyc.org
-module Web.Site.Rules.Diagram (rules, preamble) where
+--
+-- Exports the rules for generating images
+-- from [diagrams](https://diagrams.github.io/)-based Haskell code.
+module Web.Site.Rules.Diagram (rules) where
 
 import Data.ByteString.Lazy
 import Hakyll
 import Web.Site.Compilers
 
 -- |
--- Rules related to images generated from Haskell code based on Diagrams.
+-- Rules related to images generated from Haskell code based
+-- on [diagrams](https://diagrams.github.io/).
 --
--- The Haskell code should not have to import 'Diagrams.Prelude' or
--- "Diagrams.Runner.putDiagram".  These will be implicitly imported by
--- prepending boilerplate code.
---
+-- The Haskell code should not have to import "Diagrams.Prelude" or 'Diagrams.Runner.putDiagram'.
+-- These will be implicitly imported by prepending boilerplate code.
 -- This allows the Haskell code for generating diagrams to only have
 -- to define a main function which writes out the diagram, along
 -- with any other functions necessary to generate the diagram.
--- For example,
+-- The generated image will be in SVG format.
+--
+-- For example, Haskell code generating an image could look like this:
 --
 -- @
 --    main = putDiagram defaultOptions roundThing
 --
 --    roundThing :: Diagram B
 --    roundThing = circle 1 # lc red
+-- @
+--
+-- If this code is in the file @site\/diagrams\/round.hs@,
+-- this will generate the file @\/diagrams\/round.svg@ for the site.
+--
+-- Other modules can still be exported explicitly.  For example,
+--
+-- @
+--     import Diagrams.TwoD.Path.IteratedSubset
+--
+--     main = putDiagram defaultOptions example
+--
+--     example :: Diagram B
+--     example = strokeTrail $ snowflake 5
 -- @
 rules :: Rules ()
 rules = do
@@ -38,9 +56,9 @@ rules = do
 --
 -- This will be used to remove common boilerplate from Haskell code
 -- which are used to generate diagrams for this web site.
--- The preamble will import the 'Diagrams.Runner' module,
--- which basically re-exports 'Diagrams.Prelude'.
--- It will also export the "Diagrams.Runner.putDiagram" function,
+-- The preamble will import the "Diagrams.Runner" module,
+-- which basically re-exports "Diagrams.Prelude".
+-- It will also export the 'Diagrams.Runner.putDiagram' function,
 -- which will be responsible for writing out a diagram in SVG format
 -- to standard output.
 --
