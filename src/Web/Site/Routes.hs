5e154bcfd060438f910058c58657d0745a11105a
Yoo Chung <dev@chungyc.org>
2023-03-20 21:22:06 -0400

    More documentation for Web.Site.Routes module.

--- src/Web/Site/Routes.hs
+++ src/Web/Site/Routes.hs
@@ -3,10 +3,15 @@
 -- Copyright: Copyright (C) 2023 Yoo Chung
 -- License: All rights reserved
 -- Maintainer: web@chungyc.org
+--
+-- Hakyll route functions useful for this web site.
 module Web.Site.Routes (stripExtension) where
 
 import Hakyll
 
--- | Strip extension from the file name.
+-- |
+-- Strip extension from the file name.
+--
+-- It is the same as @setExtension ""@, but just with a more obvious name.
 stripExtension :: Routes
 stripExtension = setExtension ""
