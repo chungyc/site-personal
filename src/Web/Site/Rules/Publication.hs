69b33dc8424eab9be5f1888afcc6258940eb9c31
Yoo Chung <dev@chungyc.org>
2023-03-20 22:40:44 -0400

    More documentation for Web.Site.Rules.Publication module.

--- src/Web/Site/Rules/Publication.hs
+++ src/Web/Site/Rules/Publication.hs
@@ -3,10 +3,14 @@
 -- Copyright: Copyright (C) 2023 Yoo Chung
 -- License: All rights reserved
 -- Maintainer: web@chungyc.org
+--
+-- Exports rules for my list of publications.
 module Web.Site.Rules.Publication (rules, items) where
 
 import Hakyll
 
+-- |
+-- Rules related to the list of my publications.
 rules :: Rules ()
 rules = do
   match "publications/index.markdown" $ do
@@ -20,6 +24,8 @@ rules = do
 
 -- |
 -- Pattern for files matched or created in this module.
+--
+-- These will be used to generate the sitemap.
 items :: Pattern
 items = "publications/index.markdown"
 
@@ -31,8 +37,13 @@ publicationsCompiler = do
   doc <- readPandocBiblio defaultHakyllReaderOptions csl bib body
   return $ writePandoc doc
 
+-- |
+-- This is a modified version of the ACM's CSL file.
+--
+-- The difference is that this sorts by date in reverse order.
 cslFile :: String
 cslFile = "publications/acm.csl"
 
+-- | This is a bibliography of my publications.
 bibFile :: String
 bibFile = "publications/chungyc.bib"
