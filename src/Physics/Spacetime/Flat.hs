8a72c44c5f480706c49a35a06f58f63b36bb2f46
Yoo Chung <dev@chungyc.org>
2023-04-02 01:01:34 -0400

    Documentation and more operations for Physics.Spacetime.Flat module.

--- src/Physics/Spacetime/Flat.hs
+++ src/Physics/Spacetime/Flat.hs
@@ -8,28 +8,98 @@
 -- provides functions to manipulate and transform them.
 -- It is intended to be a simple module sufficient enough
 -- to assist in drawing spacetime diagrams.
-module Physics.Spacetime.Flat (Coordinate (..), interval, transform) where
+module Physics.Spacetime.Flat
+  ( -- * Coordinates
+    Coordinate (..),
+
+    -- * Simple operations
+    plus,
+    minus,
+    scale,
+    combine,
+    approximates,
+
+    -- * Spacetime interval
+    interval,
+
+    -- * Lorentz transformation
+    transform,
+
+    -- * Working with Diagrams
+    vectorize2D,
+  )
+where
+
+import Diagrams.TwoD.Types
 
 -- | The coordinates \((t, x, y, z)\) in flat spacetime.
 --
 -- The coordinates use Planck units.  In other words, \(c = 1\).
---
--- This is not of the 'Eq' type class because one should usually not
--- compare floating-point values for exact equality.
 newtype Coordinate = Coordinate (Double, Double, Double, Double)
-  deriving (Show)
+  deriving (Eq, Show)
+
+-- | Treat two coordinates like vectors and add them together.
+--
+-- >>> Coordinate (2, 1, 0, 0) `plus` Coordinate (-1, 1, 0, 0)
+-- Coordinate (1.0,2.0,0.0,0.0)
+plus :: Coordinate -> Coordinate -> Coordinate
+plus = combine (+)
+
+-- | Treat two coordinates like vectors and subtract the second one from the first.
+--
+-- >>> Coordinate (2, 1, 0, 0) `minus` Coordinate (-1, 1, 0, 0)
+-- Coordinate (3.0,0.0,0.0,0.0)
+minus :: Coordinate -> Coordinate -> Coordinate
+minus = combine (-)
+
+-- | Scale each component of a coordinate with the given function.
+--
+-- >>> scale (*2) $ Coordinate (1, 2, 0, 0)
+-- Coordinate (2.0,4.0,0.0,0.0)
+scale :: (Double -> Double) -> Coordinate -> Coordinate
+scale f (Coordinate (t, x, y, z)) = Coordinate (f t, f x, f y, f z)
+
+-- | Combine the corresponding components of two coordinates with the given function.
+--
+-- >>> combine (*) (Coordinate (2, 3, 1, 2)) (Coordinate (1, -1, 2, 3))
+-- Coordinate (2.0,-3.0,2.0,6.0)
+combine :: (Double -> Double -> Double) -> Coordinate -> Coordinate -> Coordinate
+combine f (Coordinate (t, x, y, z)) (Coordinate (t', x', y', z')) =
+  Coordinate (f t t', f x x', f y y', f z z')
+
+-- | Returns whether two coordinates are approximately equal to each other.
+--
+-- >>> Coordinate (1, 1, 0, 0) `approximates` Coordinate (1, 1, 0, 0)
+-- True
+-- >>> Coordinate (1, -1, 0, 0) `approximates` Coordinate (1, 1, 0, 0)
+-- False
+--
+-- The threshold for being approximately equivalent is somewhat arbitrary.
+approximates :: Coordinate -> Coordinate -> Bool
+approximates (Coordinate (t, x, y, z)) (Coordinate (t', x', y', z')) =
+  t `closeTo` t' && x `closeTo` x' && y `closeTo` y' && z `closeTo` z'
+  where
+    closeTo a b = abs (a - b) < 1e-5
 
 -- | The spacetime interval between the origin and the given coordinates.
 --
 -- This uses the convention where \(ds^2 = dt^2 - dx^2 - dy^2 - dz^2\).
+--
+-- >>> interval $ Coordinate (2, 1, 0, 0)
+-- 3.0
 interval :: Coordinate -> Double
 interval (Coordinate (t, x, y, z)) = t ** 2 - x ** 2 - y ** 2 - z ** 2
 
 -- | Apply the Lorentz transformation.
 --
--- This transforms coordinates in an original intertial frame to those
+-- This transforms coordinates in an original inertial frame to those
 -- in a new inertial frame.  It is assumed that the origin coincides
 -- between the two frames, and that the velocity only has an \(x\) component.
+--
+-- >>> let expected = Coordinate (0.20710678118, 0.20710678118, 0.0, 0.0)
+-- >>> let actual = transform (sqrt 0.5) $ Coordinate (1, 1, 0, 0)
+-- >>> actual `approximates` expected
+-- True
 --
 -- For reference, look up the
 -- [Lorentz transformation](https://chungyc.org/article/physics/relativity/#lorentz).
@@ -42,8 +112,19 @@ transform ::
   Coordinate
 transform v (Coordinate (t, x, y, z)) = Coordinate (t', x', y', z')
   where
-    gamma = 1 - v ** 2
+    gamma = sqrt $ 1 - v ** 2
     t' = gamma * (t - v * x)
     x' = gamma * (x - v * t)
     y' = y
     z' = z
+
+-- | Convert given coordinates into a two-dimensional "Diagrams" vector.
+--
+-- It will project the \(x\) coordinate to the \(x\) component
+-- and the \(t\) coordinate to the \(y\) component of the vector.
+-- The other coordinates will be ignored.
+--
+-- >>> vectorize2D $ Coordinate (1, 1, 0, 0)
+-- V2 1.0 1.0
+vectorize2D :: Coordinate -> V2 Double
+vectorize2D (Coordinate (t, x, _, _)) = r2 (x, t)
